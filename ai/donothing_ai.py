e3abaa36d4ed618f22bfcad8e614becf299d5ca7
Alexander Lawless <ajl429@cornell.edu>
2024-05-16 17:59:38 -0400

    Updated all ais to match config compatibility

--- ai/donothing_ai.py
+++ ai/donothing_ai.py
@@ -5,50 +5,53 @@ from gui.turbohelper import *
 
 ### DO NOTHING AI ####
 
-def getAction(state, time_left=None):
+
+def getAction(state, config, time_left=None):
     """Main AI function.  It should return a valid AI action for this state."""
 
-    #Get the possible actions in this state
+    # Get the possible actions in this state
     actions = getAllowedActions(state)
-    
-    #Select random action (to use for non-attack turn types)    
+
+    # Select random action (to use for non-attack turn types)
     myaction = random.choice(actions)
-    
-    #If turn type is attack, then choose to end attack
+
+    # If turn type is attack, then choose to end attack
     if state.turn_type == 'Attack':
-        myaction = actions[-1] #The last action is always "Stop Attacking"
-        
+        myaction = actions[-1]  # The last action is always "Stop Attacking"
+
     return myaction
 
-#Code below this is the interface with Risk.pyw GUI version
-#DO NOT MODIFY
-    
-def aiWrapper(function_name, occupying=None):
+# Code below this is the interface with Risk.pyw GUI version
+# DO NOT MODIFY
+
+
+def aiWrapper(function_name, config, occupying=None):
     game_board = createRiskBoard()
     game_state = createRiskState(game_board, function_name, occupying)
-    action = getAction(game_state)
+    action = getAction(game_state, config)
     return translateAction(game_state, action)
-            
-def Assignment(player):
-#Need to Return the name of the chosen territory
-    return aiWrapper('Assignment')
-     
-def Placement(player):
-#Need to return the name of the chosen territory
-     return aiWrapper('Placement')
 
-    
+
+def Assignment(player):
+    # Need to Return the name of the chosen territory
+    return aiWrapper('Assignment')
+
+
+def Placement(player):
+    # Need to return the name of the chosen territory
+    return aiWrapper('Placement')
+
+
 def Attack(player):
- #Need to return the name of the attacking territory, then the name of the defender territory    
+ # Need to return the name of the attacking territory, then the name of the defender territory
     return aiWrapper('Attack')
 
-   
-def Occupation(player,t1,t2):
- #Need to return the number of armies moving into new territory      
-    occupying = [t1.name,t2.name]
-    return aiWrapper('Occupation',occupying)
-   
+
+def Occupation(player, t1, t2):
+ # Need to return the number of armies moving into new territory
+    occupying = [t1.name, t2.name]
+    return aiWrapper('Occupation', occupying)
+
+
 def Fortification(player):
     return aiWrapper('Fortification')
-
-  
